745bd9c04040ca588f60dd896e40db30fccc50eb
Bruno Matias <30832455+rnbruno@users.noreply.github.com>
2017-12-06 07:40:29 -0300

    Add files via upload

---
+++ Trabalho_1_Unidade_3.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+Caros alunos(as),
+Segue a descrição do segundo trabalho da terceira unidade, cuja nota corresponde a 60% da respectiva unidade. 
+Tema: distribuições contínuas e análise de confiabilidade
+Grupo: no máximo 02 alunos
+Entregável: link para o notebook no github. No readme do github mencionar um outro link para um video no youtube descrevendo o notebook e os resultados. 
+Descrição:
+Realizar um estudo com diferentes configurações de redundância (TMR, 5MR, paralela com 2,3,4 dispositivos), valores de lambda. Identificar no estudo os valores temporais que a partir deles não se justifica o uso de redundância. Para cada cenário traçar linhas verticais indicando o MTTF de cada modelo de redundância. 
+Esse trabalho terá o maior critério de exigência da disciplina. 
+att
+Ivanovitch
+
